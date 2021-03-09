e4157975dcbb6c32bd5b60545cdd5848cd00ac91
Benjamin Pinter <ben.david.pinter@iCloud.com>
2021-03-09 12:08:04 -0500

    Implementation of 'Non Query' tests
    Finished all standard operations of non query

--- Hands.Of.Jarvis.Tests/NonQuery.cs
+++ Hands.Of.Jarvis.Tests/NonQuery.cs
@@ -16,11 +16,32 @@ namespace Hands.Of.Jarvis.Tests
             cli = new JarvisClient("../../../TestDb/TestDb.db");
         }
 
-        [Test]
-        public async Task Scalar_Returns_Neg1_On_Single_Record_Select()
+        [Test, Order(1)]
+        public async Task NQ_Returns_Neg1_On_Single_Record_Select()
         {
             var result = await cli.ExecuteNonQuery("select * from scalartester limit 1");
             Assert.IsTrue(result == -1);
+        }
+
+        [Test, Order(2)]
+        public async Task NQ_Returns_1_On_Single_Record_Insert()
+        {
+            var result = await cli.ExecuteNonQuery("insert into ScalarTester (TextColumn) values('hello!')");
+            Assert.IsTrue(result == 1);
+        }
+
+        [Test, Order(3)]
+        public async Task NQ_Returns_2_On_Multi_Record_Insert()
+        {
+            var result = await cli.ExecuteNonQuery("insert into ScalarTester (TextColumn) select 'hello!' union all select 'hello again!'");
+            Assert.IsTrue(result == 2);
+        }
+
+        [Test, Order(4)]
+        public async Task NQ_Returns_GT1_On_Delete()
+        {
+            var result = await cli.ExecuteNonQuery("delete from Scalartester");
+            Assert.IsTrue(result > 2);
         }
     }
 }
